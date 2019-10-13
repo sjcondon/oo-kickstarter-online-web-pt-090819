747b5a815cefb876b01bc41a44d34500ffcf8a61
sarah <s.j.condon01@gmail.com>
2019-10-13 03:51:29 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,8 @@
+class Baker
+  attr_reader :projects
+  def initialized
+    @backs_project = []
+  end
+  def back_projects()
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,8 @@
+class Projects
+  attr_reader :backers
+  def initialized(name)
+    @backer = []
+  end
+  def add_backer()
+  end
+end
