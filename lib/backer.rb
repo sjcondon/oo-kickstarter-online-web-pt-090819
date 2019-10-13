03db8bd6e66552e56a5222a32c042e8d7003118f
sarah <s.j.condon01@gmail.com>
2019-10-13 03:52:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,7 @@
 class Baker
-  attr_reader :projects
-  def initialized
+  attr_reader :projects, :name
+  def initialized(name)
+    @name = name
     @backs_project = []
   end
   def back_projects()
